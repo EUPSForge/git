2a468e1ac5af739dc1b4f496986ce27792686976
Mario Juric <mjuric@lsst.org>
2015-05-06 18:27:29 -0500

    ups/eupspkg.cfg.sh: hack around libcurl link problems with Anaconda

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -4,6 +4,27 @@
 # are picked up. This is needed both by build() and install()
 export CURLDIR=$(curl-config --prefix)
 
+#
+# Hack around a problem where the user may have curl on $PATH that is linked
+# against a dynamic library that is not on LD_LIBRARY_PATH.  While libcurl
+# itself will get picked up because of the -L....  option, the libraries
+# that libcurl is linked against (e.g., libssl), won't, thus leading either
+# to a failed build, or a failure to detect curl (and thus the lack of
+# http/https support).  This happens with Anaconda (which distributes its
+# own curl).
+#
+# We try to detect and work around this by looking at whether there's a -L
+# stanza in curl-config --libs, and specifying an explicit LD_RUN_PATH if
+# there is one.
+#
+LIBFLAGS=$(curl-config --libs)
+if [[ $LIBFLAGS == -L* ]]; then
+	lf=${LIBFLAGS#-L}
+        lf=${lf%% *}
+	export LD_RUN_PATH="$lf:$LD_RUN_PATH"
+	echo "$LD_RUN_PATH"
+fi
+
 build()
 {
 	export NO_FINK=1
